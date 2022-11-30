52ca3f43b157d94a67632c97243c9c1d23280d2f
Carlos C <95592181+xaxlxs@users.noreply.github.com>
2022-11-30 04:35:58 +0000

    Updates

--- Automating-With-Python/Scale-and-Convert/Scale-and-convert-using-PIL.py
+++ Automating-With-Python/Scale-and-Convert/Scale-and-convert-using-PIL.py
@@ -1,9 +1,9 @@
 from PIL import Image
-# import os
-# import sys
+import os
+import sys
 
 # source = os.path.join(sys.path[0], "IMG_1020s.png")
-source = "TestF/"
+source = "/TestF/"
 output = "/opt/icons/"
 
 for file in source:
