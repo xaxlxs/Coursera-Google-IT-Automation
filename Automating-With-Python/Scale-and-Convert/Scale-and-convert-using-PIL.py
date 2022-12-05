ed0034cf93ea58521f3c6eab111263ef4e1ac607
Carlos C <95592181+xaxlxs@users.noreply.github.com>
2022-12-05 15:31:59 +0000

    Update script

--- Automating-With-Python/Scale-and-Convert/Scale-and-convert-using-PIL.py
+++ Automating-With-Python/Scale-and-Convert/Scale-and-convert-using-PIL.py
@@ -1,11 +1,36 @@
-from PIL import Image
+#!/usr/bin/env python3
+
 import os
+import glob
 import sys
+from PIL import Image
 
 # source = os.path.join(sys.path[0], "IMG_1020s.png")
-source = "/TestF/"
+
+# image1 = Image.open(source + "ic_map_black_48dp")
+# image1.save("newblack_48dp.jpg")
+
+source = "/images/"
 output = "/opt/icons/"
+size = 128, 128
 
 for file in source:
-    im = Image.open(file)
-    im.save(output + file, "JPEG")
+    with Image.open(file) as im:
+
+        # Hide the below lines to test
+        im = im.rotate(270)
+        im = im.resize((size))
+        # Hide the above lines to test
+        # im = im.convert("RGB")
+
+        im.save(output + file, "JPEG")
+        
+
+
+
+"""
+Psudo:
+Rotate the image 90° clockwise
+Resize the image from 192x192 to 128x128
+Save the image in /opt/icons/ as .jpeg format
+"""
