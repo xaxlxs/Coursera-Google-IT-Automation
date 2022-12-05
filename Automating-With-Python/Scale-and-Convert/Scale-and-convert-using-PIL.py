b3a3b0c83a2656a4d592b14a27e3dd58c2da0f61
Carlos C <95592181+xaxlxs@users.noreply.github.com>
2022-12-05 11:30:52 -0800

    Update Scale-and-convert-using-PIL.py

--- Automating-With-Python/Scale-and-Convert/Scale-and-convert-using-PIL.py
+++ Automating-With-Python/Scale-and-Convert/Scale-and-convert-using-PIL.py
@@ -5,32 +5,26 @@ import glob
 import sys
 from PIL import Image
 
-# source = os.path.join(sys.path[0], "IMG_1020s.png")
-
-# image1 = Image.open(source + "ic_map_black_48dp")
-# image1.save("newblack_48dp.jpg")
-
-source = "/images/"
-output = "/opt/icons/"
-size = 128, 128
-
-for file in source:
-    with Image.open(file) as im:
-
-        # Hide the below lines to test
-        im = im.rotate(270)
-        im = im.resize((size))
-        # Hide the above lines to test
-        # im = im.convert("RGB")
-
-        im.save(output + file, "JPEG")
-        
-
-
-
 """
 Psudo:
 Rotate the image 90° clockwise
 Resize the image from 192x192 to 128x128
 Save the image in /opt/icons/ as .jpeg format
 """
+
+# source = os.path.join(sys.path[0], "IMG_1020s.png")
+# image1 = Image.open(source + "ic_map_black_48dp")
+# image1.save("newblack_48dp.jpg")
+
+source = "/images/"
+output = "/opt/icons/"
+rotation = 270
+size = 128, 128
+
+for file in source:
+    im = Image.open(file)
+    im.rotate(rotation).resize((size)).save(output + file, "JPEG")
+    
+    # Hide the below lines to test
+    # im = im.convert("RGB")    
+    # Hide the above lines to test
