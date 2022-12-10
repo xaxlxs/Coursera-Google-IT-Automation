cb4a3dd7646bed4fea3fa63f326623e3156342ed
Carlos C <95592181+xaxlxs@users.noreply.github.com>
2022-12-10 19:44:17 +0000

    Update Scale script

--- Automating-With-Python/Scale-and-Convert/TestF/Test.py
+++ Automating-With-Python/Scale-and-Convert/TestF/Test.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python3
 
-import os
-from PIL import Image
-
-# print(os.getcwd())
+# import os
+# from PIL import Image
 
 # im = Image.open("ic_map_black_48dp").convert('RGB')
 # im.save("new.jpeg", "JPEG")
 
 
-size = (128, 128)
+# size = (128, 128)
 
-for file in os.listdir():
-    output = os.path.splitext(file)[0]
-    try:
-        im = Image.open(file).convert('RGB')
-        im.save(output, "JPEG")
-    except OSError:
-        pass
+# for file in os.listdir():
+#     output = os.path.splitext(file)[0]
+#     try:
+#         im = Image.open(file)
+#         im.convert('RGB')
+#         im.save(output, "JPEG")
+#     except OSError:
+#         pass
 
 # import os, glob
 # from PIL import Image
@@ -27,3 +26,15 @@ for file in os.listdir():
 # for file in glob.glob("ic_*"):
 #     im = Image.open(file).convert('RGB')
 #     im.rotate(270).resize((newsize)).save(file, "JPEG")
+
+#!/usr/bin/env python3
+import os
+from PIL import Image
+import glob
+
+os.mkdir("/workspaces/Coursera-Google-IT-Automation/Automating-With-Python/Scale-and-Convert/opt/")
+os.mkdir("/workspaces/Coursera-Google-IT-Automation/Automating-With-Python/Scale-and-Convert/opt/icons/")
+
+for file in glob.glob('ic_*'):
+	im = Image.open(file).convert('RGB')
+	im.rotate(270).resize((128,128)).save("/workspaces/Coursera-Google-IT-Automation/Automating-With-Python/Scale-and-Convert/opt/icons/" + str(file) + ".jpg")
