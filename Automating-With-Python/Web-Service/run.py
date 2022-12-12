663395a6c7b6fe5a0a451a8c270b720ef7ef534e
Carlos C <95592181+xaxlxs@users.noreply.github.com>
2022-12-12 03:28:31 +0000

    Finished Uploading to Web Server script

--- Automating-With-Python/Web-Service/run.py
+++ Automating-With-Python/Web-Service/run.py
@@ -8,11 +8,17 @@ address = "http://<corpweb-external-IP>/feedback"
 relative_path = "data/feedback"
 source = os.path.join(os.getcwd(), relative_path)
 
+# os.chdir("..")
+# os.chdir("..")
+
+# to get script to work on qwiklabs, uncomment 2 chdir commands above, change source belot to relative_path
+
 for file in os.listdir(source):
     with open(os.path.join(source, file)) as reader:
         title, name, date, review = reader.readlines()
         dict = {"title": title.strip(), "name": name.strip(), "date": date.strip(), "feedback": review.strip()}
-        print(dict)
+        review_json = json.dumps(dict)
+        # print(review_json)
 
-        response = requests.post(address, data=dict)
+        response = requests.post(address, json=dict)
         print(response.status_code)
