c4a37079864db4e5a5a3520685e4fe6673816c10
Carlos C <95592181+xaxlxs@users.noreply.github.com>
2022-12-17 19:50:25 +0000

    Finished Practice-PDF 3

--- Automating-With-Python/Email-PDF/Practice-PDF.py
+++ Automating-With-Python/Email-PDF/Practice-PDF.py
@@ -20,6 +20,8 @@ report_title = Paragraph("A Complete Inventory of My Fruit", styles["h1"])
 
 from reportlab.lib import colors
 
+# Dictionary of data for table
+
 fruit = {
   "elderberries": 1,
   "figs": 1,
@@ -30,13 +32,41 @@ fruit = {
   "grapes": 13
 }
 
+# Turn data dictionary into a list of lists (array)
+
 table_data = []
 for k, v in fruit.items():
     table_data.append([k, v])
 
 # print(table_data)
 
+# Create the table
+
 table_style = [('GRID', (0,0), (-1,-1), 1, colors.black)]
 report_table = Table(data=table_data, style=table_style, hAlign="LEFT")
 
-report.build([report_title, report_table])
+# report.build([report_title, report_table])
+
+
+#######################
+# ADDING A PIE CHART
+#######################
+
+from reportlab.graphics.shapes import Drawing
+from reportlab.graphics.charts.piecharts import Pie
+from reportlab.lib.pagesizes import inch
+
+report_pie = Pie(width=3*inch, height=3*inch)
+
+report_pie.data = []
+report_pie.labels = []
+for name in sorted(fruit):
+    report_pie.data.append(fruit[name])
+    report_pie.labels.append(name)
+
+# print(report_pie.data, "\n", report_pie.labels)
+
+report_chart = Drawing()
+report_chart.add(report_pie)
+
+report.build([report_title, report_table, report_chart])
