45ae65e87dfc70393f909f7704161304f54d4dd9
Carlos C <95592181+xaxlxs@users.noreply.github.com>
2022-12-17 19:06:46 +0000

    Start Practice PDF

---
+++ Automating-With-Python/Email-PDF/Practice-PDF.py
@@ -0,0 +1,18 @@
+from reportlab.platypus import SimpleDocTemplate
+from reportlab.platypus import Paragraph, Spacer, Table, Image
+from reportlab.lib.styles import getSampleStyleSheet
+
+fruit = {
+  "elderberries": 1,
+  "figs": 1,
+  "apples": 2,
+  "durians": 3,
+  "bananas": 5,
+  "cherries": 8,
+  "grapes": 13
+}
+
+report = SimpleDocTemplate("report.pdf")
+styles = getSampleStyleSheet()
+report_title = Paragraph("A Complete Inventory of My Fruit", styles["h1"])
+report.build([report_title])
