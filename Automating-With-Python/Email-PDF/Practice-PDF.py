27799478f7c037f7db5a2e65b8acb81997371461
Carlos C <95592181+xaxlxs@users.noreply.github.com>
2022-12-17 19:17:56 +0000

    Finish Practice-PDF 2

--- Automating-With-Python/Email-PDF/Practice-PDF.py
+++ Automating-With-Python/Email-PDF/Practice-PDF.py
@@ -1,6 +1,7 @@
 from reportlab.platypus import SimpleDocTemplate
 from reportlab.platypus import Paragraph, Spacer, Table, Image
 from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib import colors
 
 fruit = {
   "elderberries": 1,
@@ -12,7 +13,18 @@ fruit = {
   "grapes": 13
 }
 
+table_data = []
+for k, v in fruit.items():
+    table_data.append([k, v])
+
+# print(table_data)
+
 report = SimpleDocTemplate("report.pdf")
 styles = getSampleStyleSheet()
+
 report_title = Paragraph("A Complete Inventory of My Fruit", styles["h1"])
-report.build([report_title])
+
+table_style = [('GRID', (0,0), (-1,-1), 1, colors.black)]
+report_table = Table(data=table_data, style=table_style, hAlign="LEFT")
+
+report.build([report_title, report_table])
