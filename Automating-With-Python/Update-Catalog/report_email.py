c77b82d52ff3629c7fdb29507c3fc5968a4c009f
Carlos C <95592181+xaxlxs@users.noreply.github.com>
2022-12-18 23:04:29 +0000

    Update report email

--- Automating-With-Python/Update-Catalog/report_email.py
+++ Automating-With-Python/Update-Catalog/report_email.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python3
 
 import os
+import glob
 from datetime import datetime
+import json
 import reports
+
+# Verify source location in Qwiklabs
+source = "supplier-data/descriptions"
+
+os.chdir(source)
+
+for file in glob.glob("*.txt"):
+    with open(file) as reader:
+        name, ext = os.path.splitext(file)
+        image_file = name + ".jpeg"
+        # print(reader.readlines())
+        name = reader.readline()
+        weight = reader.readline()
+        dict = {"name": name.strip(), "weight": int(weight.strip(" lbs\n"))}
+        data_json = json.dumps(dict)
+        print(data_json)
