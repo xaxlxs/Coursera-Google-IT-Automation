4c83a2ca08a7e88b86d6b089f1f9f6326e04ca5d
Carlos C <95592181+xaxlxs@users.noreply.github.com>
2022-12-18 22:40:07 +0000

    Update reports.py

--- Automating-With-Python/Update-Catalog/reports.py
+++ Automating-With-Python/Update-Catalog/reports.py
@@ -5,10 +5,10 @@ from reportlab.platypus import SimpleDocTemplate
 from reportlab.platypus import Paragraph, Spacer
 from reportlab.lib.styles import getSampleStyleSheet
 
-def generate_report(title, additional_info):
-    filename = "processed.pdf"
+def generate_report(attachment, title, paragraph):
+    # attachment = "processed.pdf"
     styles = getSampleStyleSheet()
-    report = SimpleDocTemplate(filename)    
+    report = SimpleDocTemplate(attachment)    
     report_title = Paragraph(title, styles["h1"])
-    report_info = Paragraph(additional_info, styles["BodyText"])
+    report_info = Paragraph(paragraph, styles["BodyText"])
     report.build([report_title, report_info])
