79a78a1687151e3f22c3aa89c600fec810190014
Carlos C <95592181+xaxlxs@users.noreply.github.com>
2022-12-19 00:12:35 +0000

    Update report email script

--- Automating-With-Python/Update-Catalog/report_email.py
+++ Automating-With-Python/Update-Catalog/report_email.py
@@ -1,22 +1,39 @@
 #!/usr/bin/env python3
 
+import sys
 import os
 import glob
 from datetime import datetime
 import reports
 
 # Verify source location in Qwiklabs
-source = "supplier-data/descriptions"
+source = "supplier-data/descriptions/"
 
-os.chdir(source)
+def process_data():
+    paragraph = ""
+    for file in glob.glob("*.txt", root_dir=source):
+        with open(source + file) as reader:
+            # print(reader.readlines())
+            name = reader.readline().strip()
+            weight = reader.readline().strip()
+            # print(name, weight)
+            paragraph += "name: " + name + "\n" + "weight: " + weight + "\n\n"
+    return paragraph
 
-paragraph = ""
 
-for file in glob.glob("*.txt"):
-    with open(file) as reader:
-        # print(reader.readlines())
-        name = reader.readline().strip()
-        weight = reader.readline().strip()
-        # print(name, weight)
-        paragraph += "name: " + name + "\n" + "weight: " + weight + "\n\n"
-print(paragraph)
+def main(argv):
+    data = process_data()
+    # print(data)
+
+    # Update attachment path in Qwiklab: /tmp/processed.pdf
+    attachment = "processed.pdf"
+
+    today = datetime.now()
+    date = today.strftime("%B %d, %Y")
+    title = "Processed Update on " + date
+    reports.generate_report(attachment, title, data)
+
+
+
+if __name__ == "__main__":
+    main(sys.argv)
